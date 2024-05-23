e2f55247b14188a8fe2c2bb200b75e3fca6d0a1f
RubenFuquene <rubendariofuquene@gmailcom>
2024-05-22 23:12:02 -0500

    first commit

---
+++ src/main/java/com/gruposeleccion/app/ModuloseleccionrestApplication.java
@@ -0,0 +1,13 @@
+package com.gruposeleccion.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloseleccionrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloseleccionrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gruposeleccion/app/ModuloseleccionrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruposeleccion.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloseleccionrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
